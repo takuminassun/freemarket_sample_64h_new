526659685f75fc024ec459c45084900ec319457c
atsu0203 <atsu0203@gmail.com>
2019-12-05 12:12:58 +0900

    add-slick-and-carousel

---
+++ app/assets/javascripts/channels/slick.js
@@ -0,0 +1,6 @@
+$(function(){
+  $("#slider").slick({
+    prevArrow: '<img src="前への矢印画像のパス" class="slide-arrow prev-arrow">',
+    nextArrow: '<img src="次への矢印画像のパス" class="slide-arrow next-arrow">'
+  });
+});
