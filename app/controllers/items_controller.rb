0100e53fa519afd19119b8027a52dbeb18ba1d5d
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2019-12-10 19:55:07 +0900

    form-create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  def index
+  end
+
   def new 
     @item = Item.new
   end
@@ -9,5 +12,6 @@ class ItemsController < ApplicationController
       redirect_to "/"
     else
       render "new"
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_025141) do
+ActiveRecord::Schema.define(version: 2019_12_10_083417) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_025141) do
     t.integer "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "image_id"
     t.index ["name"], name: "index_items_on_name", unique: true
   end
 
