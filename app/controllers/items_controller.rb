7260596416b795b57bbbe8cc0a7b1e5984df58f7
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2019-12-10 15:38:24 +0900

    file change

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+  def new 
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to "/"
+    else
+      render "new"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :items, only: [:index, :new, :create]
   root to: 'posts#index'
 end
