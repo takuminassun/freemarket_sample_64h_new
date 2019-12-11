9abb3857994685a2ec77187c372d807b666618d7
jenettosyo <jenettosyo@gmail.com>
2019-12-11 19:32:35 +0900

    select

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,4 @@
 class Address < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
+  belongs_to :user
+  belongs_to :prefecture
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_052528) do
+ActiveRecord::Schema.define(version: 2019_12_11_063649) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post_number", null: false
+    t.string "city", null: false
+    t.string "town", null: false
+    t.string "building"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,7 +35,7 @@ ActiveRecord::Schema.define(version: 2019_12_05_052528) do
     t.integer "born_year", null: false
     t.integer "born_month", null: false
     t.integer "born_day", null: false
-    t.integer "phone_number", null: false
+    t.string "phone_number", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
