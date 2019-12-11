3d8d25645b8a346dbd58f77c7bd98f5580819d32
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2019-12-11 15:54:37 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
+  belongs_to_active_hash :prefecture
 end
