edc13ed94b27ed58f0cff3708fc0c07b199f9139
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2019-12-11 12:56:33 +0900

    path_setting

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/product-listing', to: "posts#product-listing"
   get '/purchase', to: "posts#purchase"
   get '/show', to: "posts#show"
+  get '/login', to: "devise/registration#login"
   root to: 'posts#index'
 
   
