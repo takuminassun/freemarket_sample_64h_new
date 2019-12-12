34e1fad995a8d1165b341d1de5eb012be58a8a39
atsu0203 <atsu0203@gmail.com>
2019-12-12 15:09:48 +0900

    add-create-image

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,21 +4,28 @@ class ItemsController < ApplicationController
 
   def new 
     @item = Item.new
-    @item.images.build
+    @image = @item.images.build
   end
 
   def create
-    @item = Item.new(item_params)exi
+    @item = Item.new(item_params)
     if @item.save
-      redirect_to "/"
+      # params[:images][:image].each do |image|
+      #   @image = @item.images.create(image: image, item_id: @item.id)
+      # end
+      redirect_to root_path, notice: '出品しました。'
     else
       render "new"
     end
   end
 
+
   private
 
   def item_params
-    params.require(:item).permit(:name, :category_id, :price, :status, :description, :shipping_burden, :shipping_date, :prefecture_id, images_attributes: [:image]).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :category_id, :price, :status, :description, :shipping_burden, :shipping_date, :prefecture_id, images_attributes: [:id, :item_id, :image]).merge(user_id: current_user.id)
   end
+
+
+
 end
