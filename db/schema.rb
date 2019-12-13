05c7a20fa7627e26195dd93eea4d85dc9acb8e58
atsu0203 <atsu0203@gmail.com>
2019-12-13 12:10:14 +0900

    db-fix-name-no-unue-and-staus-string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_12_11_082135) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
