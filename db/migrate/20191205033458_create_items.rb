81adadab79dd6c138707cdd8085dca7771e46d95
atsu0203 <atsu0203@gmail.com>
2019-12-13 12:10:14 +0900

    db-fix-name-no-unue-and-staus-string

--- db/migrate/20191205033458_create_items.rb
+++ db/migrate/20191205033458_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.integer :category_id,     null: false, foreign_key: true
       t.integer :brand_id,        foreign_key: true
       t.integer :price,           null: false
-      t.integer :status,          null: false
+      t.string :status,          null: false
       t.text    :description,     null: false
       t.string :shipping_burden, null: false
       t.string :shipping_method
@@ -14,6 +14,6 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.integer :size
       t.timestamps
     end
-    add_index :items, :name,  unique: true
+    add_index :items, :name,  
   end
 end
