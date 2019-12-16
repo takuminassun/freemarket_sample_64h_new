77506834bd660cecc72eb07eeeed6a75c875df11
taku-expert <prime.stkvkp@gmail.com>
2019-12-16 20:01:54 +0900

    Revert "gem payjpの導入"
    This reverts commit 3470b7bd4046fab43bb69f2f89f107908e355c3f.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_16_030523) do
+ActiveRecord::Schema.define(version: 2019_12_12_024854) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "addresses_first_name", null: false
@@ -22,14 +22,6 @@ ActiveRecord::Schema.define(version: 2019_12_16_030523) do
     t.string "town", null: false
     t.string "building"
     t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "customer_id"
-    t.string "card_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
