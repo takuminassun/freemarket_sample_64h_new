c032d1e8db3dbcf8fb4eef4688a840df52015f8b
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2019-12-16 14:22:45 +0900

    prefix commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @users = User.all
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
-class UsersController < ApplicationController
-  def index
-  end
-
-  
+class UsersController < ApplicationController  
   def show
     @users = User.all
   end
