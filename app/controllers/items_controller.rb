265eb19e9d55f3edae2b2712effb62db932a868f
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2019-12-18 18:40:39 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only:[:show, :edit, :update, :destroy]
-  before_action :correct_user, only:[:edit, :update]
+  before_action :correct_user, only: :edit
 
   def index
 
