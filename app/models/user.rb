8fe3dce2573a8e9312218361db72775a682e2d3e
jenettosyo <jenettosyo@gmail.com>
2019-12-18 16:25:00 +0900

    facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,41 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable,omniauth_providers: [:facebook, :google_oauth2]
 
   has_one :address
   has_one :profile
   has_many :items
+  has_many :sns_credentials, dependent: :destroy
+
+  def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    if snscredential.present?
+      user = User.where(id: snscredential.user_id).first
+    else
+      user = User.where(email: auth.info.email).first
+      if user.present?
+        SnsCredential.create(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+          )
+      else
+        user = User.create(
+          sns_name: auth.info.name,
+          email:    auth.info.email,
+          password: Devise.friendly_token[0, 20],
+          )
+        SnsCredential.create(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+          )
+      end
+    end
+    return user
+  end
 end
