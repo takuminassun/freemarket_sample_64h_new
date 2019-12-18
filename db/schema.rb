99caa2d99535799a0a13edb9250aa3cd127e1713
taku-expert <prime.stkvkp@gmail.com>
2019-12-18 12:08:56 +0900

    本番環境でのエラー対応後

--- db/schema.rb
+++ db/schema.rb
@@ -26,15 +26,6 @@ ActiveRecord::Schema.define(version: 2019_12_12_024854) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "avatar"
-    t.text "comment"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
