4a1d74a692c073d1221252e098225dc9405af13c
hirotodukan <57314296+hirotodukan@users.noreply.github.com>
2019-12-19 18:27:27 +0900

    プルダウンリスト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,25 +3,13 @@ class ItemsController < ApplicationController
   before_action :correct_user, only: :edit
 
   def index
-    @categories = Category.where(params[:id])
-    @category_children1 = Category.where(ancestry:1)
-    @category_children2 = Category.where(ancestry:104)
-    @category_children3 = Category.where(ancestry:156)
-    @category_children4 = Category.where(ancestry:267)
-    @category_children5 = Category.where(ancestry:386)
-    @category_children6 = Category.where(ancestry:438)
-    @category_children7 = Category.where(ancestry:521)
-    @category_children8 = Category.where(ancestry:605)
-    @category_children9 = Category.where(ancestry:680)
-    @category_children10 = Category.where(ancestry:775)
-    @category_children11 = Category.where(ancestry:818)
-    @category_children12 = Category.where(ancestry:869)
-    @category_children13 = Category.where(ancestry:926)
     @items = Item.order("created_at DESC").page(params[:page]).per(8)
     @images = Image.all
     @categorys = Category.limit(4)
 
   end
+
+  
 
   def new 
     @item = Item.new
