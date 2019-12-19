7bec4c64fd0319b79a47decc0fcf7003ec5c9ac4
hirotodukan <57314296+hirotodukan@users.noreply.github.com>
2019-12-19 18:05:06 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
 
   private
 
