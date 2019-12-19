4b87e2941e92fc11c55b2adca8d47fd0acdf0d99
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2019-12-19 23:34:47 +0900

    edit_fix

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).on("turbolinks:load", function(){
   var slider = ".item__image__box__main"; // スライダー
   var thumbnailItem = ".item__image__box__sub__img"; // サムネイル画像アイテム
   
@@ -7,32 +7,14 @@ $(function(){
    var index = $(thumbnailItem).index(this);
    $(this).attr("data-index",index);
   });
-  
-  // スライダー初期化後、カレントのサムネイル画像にクラス「thumbnail-current」を付ける
-  // 「slickスライダー作成」の前にこの記述は書いてください。
-  $(slider).on('init',function(slick){
-   var index = $(".slide-item.slick-slide.slick-current").attr("data-slick-index");
-   $(thumbnailItem+'[data-index="'+index+'"]').addClass("thumbnail-current");
-  });
-
   //slickスライダー初期化  
   $(slider).slick({
-    autoplay: true,
     arrows: false,
-    fade: true,
-    infinite: false //これはつけましょう。
   });
   //サムネイル画像アイテムをクリックしたときにスライダー切り替え
   $(thumbnailItem).on('click',function(){
     var index = $(this).attr("data-index");
     $(slider).slick("slickGoTo",index,false);
   });
-  
-  //サムネイル画像のカレントを切り替え
-  $(slider).on('beforeChange',function(event,slick, currentSlide,nextSlide){
-    $(thumbnailItem).each(function(){
-      $(this).removeClass("thumbnail-current");
-    });
-    $(thumbnailItem+'[data-index="'+nextSlide+'"]').addClass("thumbnail-current");
-  });
+
 });
