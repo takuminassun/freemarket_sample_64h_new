cfdd4149abb1c3bf2d2ea1574c58ec0361a69e23
hirotodukan <57314296+hirotodukan@users.noreply.github.com>
2019-12-20 20:31:11 +0900

    payjp

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,7 +1,7 @@
 document.addEventListener(
   "DOMContentLoaded", e =>{
     if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
-      Payjp.setPublicKey("pk_test_3a51b8812ac66d0a028f25c3"); //公開鍵直書き
+      Payjp.setPublicKey("pk_test_e3999572a348afbbe0d9d3a7"); //公開鍵直書き
       let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得される
       btn.addEventListener("click", e => { //ボタンが押されてイベント発火
         e.preventDefault();
