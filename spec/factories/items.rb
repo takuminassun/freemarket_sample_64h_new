f647744bcd3cc48d228702f2322822ec3a95abfa
atsu0203 <atsu0203@gmail.com>
2019-12-23 20:50:05 +0900

    test-rspec

---
+++ spec/factories/items.rb
@@ -0,0 +1,14 @@
+# FactoryBot.define do
+
+#   factory :item do
+#     name              {"ごはん"}
+#     price                 {"2000"}
+#     status              {"未使用に近い"}
+#     description        {"とてもおいしいごはんです"}
+#     shipping_burden      {"送料込み"}
+#     shipping_method  {"未定"}
+#     shipping_date  {"2、3日で発送"}
+#     prefecture_id {"2"]
+#   end
+
+# end 
