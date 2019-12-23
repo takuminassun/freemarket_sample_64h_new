f59521a9497f1612abfdb25ea40d57420e4d8dd8
jenettosyo <jenettosyo@gmail.com>
2019-12-23 17:12:03 +0900

    bundleinstall

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              {"abe"}
+    email                 {"kkk@gmail.com"}
+    password              {"12345abcd"}
+    password_confirmation {"12345abcd"}
+  end
+
+end
