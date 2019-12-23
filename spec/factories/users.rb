b36fb12183479b7407092c6c6bb8f7dd19df0e36
jenettosyo <jenettosyo@gmail.com>
2019-12-23 19:32:00 +0900

    start_test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,16 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"abe"}
     email                 {"kkk@gmail.com"}
     password              {"12345abcd"}
-    password_confirmation {"12345abcd"}
+    first_name            {"田中"}
+    last_name             {"彩"}
+    first_name_kana       {"タナカ"}
+    last_name_kana        {"アヤ"}
+    born_year             {"2001"}
+    born_month            {"3"}
+    born_day              {"28"}
+    phone_number          {"08012345678"}
   end
 
 end
--- spec/helpers/card_helper_spec.rb
+++ spec/helpers/card_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe CardHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe CardHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
